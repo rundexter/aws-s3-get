9f36d9919c582a80b83606a0bfa2fbbe8b29953d
Mike Friedman <friedo@friedo.com>
2016-05-13 13:43:59 -0400

    initial

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var _ = require( 'lodash' );
+var aws = require( 'aws-sdk' );
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +9,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
-    }
-};
+      var aws_id          = dexter.environment( 'aws_access_key_id' );
+      var aws_secret      = dexter.environment( 'aws_secret_key' );
+
+      var bucket          = step.input( 'bucket' ).first();
+      var key             = step.input( 'key' ).first();
+
+      aws.config.update( { accessKeyId: aws_id, secretAccessKey: aws_secret } );
+
+      var s3 = new aws.S3();
+      var self = this;
+
+      var params = {
+          Bucket: bucket,
+          Key: key,
+      };
+
+      s3.getObject( params, function( err, data ) {
+          if ( err ) { return self.fail( err ) }
+          else       { return self.complete( data.Body.toString() ) }
+      } );
+  }
+}
